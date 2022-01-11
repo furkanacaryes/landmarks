8f908620081b8887732bb4e5a3ae5d1b61eaecf4
Furkan Acar <furkan@acar.digital>
2022-01-12 01:10:44 +0300

    completed chapter 1

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,18 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Hello, **Furkan!**")
-                .font(.title)
-                .foregroundColor(.blue)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y: -180)
+                .padding(.bottom, -180)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.green)
+
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+
+                Text("Descriptive text goes here.")
             }
-        }.padding()
+            .padding()
+            
+            Spacer()
+        }
     }
 }
 
