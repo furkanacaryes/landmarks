65a8f4cd50c547019cb272fe802159c8f121b02f
futuredayv <furkan@acar.digital>
2022-04-29 20:02:54 +0300

    feat(LandmarkDetail): render favorite button and add required bindings

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
 
     var body: some View {
         ScrollView {
@@ -21,10 +26,13 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -180)
 
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.green)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
 
                 HStack {
                     Text(landmark.park)
@@ -52,7 +60,10 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
     static var previews: some View {
-        LandmarkDetail(landmark: ModelData().landmarks[0])
+        LandmarkDetail(landmark: ModelData().landmarks[10])
+            .environmentObject(modelData)
     }
 }
