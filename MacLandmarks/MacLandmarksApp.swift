427472f3a5f550cab8b2f6dd0d80071ec41f4bb3
futuredayv <furkan@acar.digital>
2022-04-29 12:24:36 +0300

    dev: add macOS target

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Furkan on 29.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
