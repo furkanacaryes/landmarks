f462c202ccb4c604dc66ee34db34df2ad3363678
futuredayv <furkan@acar.digital>
2022-04-30 06:32:40 +0300

    feat(Categories, Landmark): add categories screen, add category codable into Landmark

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Furkan on 30.04.2022.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { categoryTitle in
+                    Text(categoryTitle)
+                }
+            }
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
