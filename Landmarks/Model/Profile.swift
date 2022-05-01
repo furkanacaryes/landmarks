d6a28867e3646f70d6cb209faffa1f828bf21e73
futuredayv <furkan@acar.digital>
2022-05-01 07:49:43 +0300

    feat(Profile): add Profile model

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Furkan on 1.05.2022.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "futuredayv")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { rawValue }
+    }
+}
