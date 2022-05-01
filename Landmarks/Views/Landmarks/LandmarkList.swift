06051200ba49122e0fa7d14b32f485ffa774d59a
futuredayv <furkan@acar.digital>
2022-05-01 14:13:51 +0300

    feat(LandmarkList): add dynamic title and filtering

--- Landmarks/Views/Landmarks/LandmarkList.swift
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -10,22 +10,32 @@ import SwiftUI
 struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
+    @State private var filter = FilterCategory.all
+    
+    enum FilterCategory: String, CaseIterable, Identifiable {
+        case all = "All"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+        
+        var id: FilterCategory { self }
+    }
     
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             !showFavoritesOnly || landmark.isFavorite
+                && (filter == .all || filter.rawValue == landmark.category.rawValue)
         }
+    }
+    
+    var title: String {
+        let title = filter == .all ? "Landmarks" : filter.rawValue
+        return showFavoritesOnly ? "Favorite \(title)" : title
     }
 
     var body: some View {
         NavigationView {
             List {
-                Section {
-                    Toggle(isOn: $showFavoritesOnly) {
-                        Text("Show Only Favorites")
-                    }
-                }
-
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink {
                         LandmarkDetail(landmark: landmark)
@@ -34,18 +44,35 @@ struct LandmarkList: View {
                     }
                 }
             }
-            .navigationTitle("Landmarks")
+            .navigationTitle(title)
+            .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
+                            }
+                        }
+                        .pickerStyle(.inline)
+
+                        Toggle(isOn: $showFavoritesOnly) {
+                            Text("Show Only Favorites")
+                        }
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
+            
+            Text("Select a landmark")
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-           LandmarkList()
-               .previewDevice(PreviewDevice(rawValue: deviceName))
-               .previewDisplayName(deviceName)
-        }
-        .environmentObject(ModelData())
+        LandmarkList()
+            .environmentObject(ModelData())
     }
 }
