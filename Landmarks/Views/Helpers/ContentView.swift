966e2174373771f18d146ae65a4c52007cc8116d
futuredayv <furkan@acar.digital>
2022-05-01 05:28:37 +0300

    feat(Routing): wrap CategoryHome and LandmarkList into a TabView, and CategoryItems into a NavigationLink

--- Landmarks/Views/Helpers/ContentView.swift
+++ Landmarks/Views/Helpers/ContentView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tag(Tab.featured)
+                .tabItem({
+                    Label("Featured", systemImage: "star")
+                })
+
+            LandmarkList()
+                .tag(Tab.list)
+                .tabItem({
+                    Label("List", systemImage: "list.bullet")
+                })
+        }
     }
 }
 
